88572dcda563a2568e2f9af05e88193e29cc3515
Tahir Murata <147841049+pastc@users.noreply.github.com>
2024-10-18 20:22:08 +0900

    Sorted battery view

--- src/views/BatteryView.vue
+++ src/views/BatteryView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useBatteryStore } from '@/stores/battery';
 import { useIntervalFn } from '@vueuse/core';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
 const batteryStore = useBatteryStore()
 
@@ -15,14 +15,23 @@ onMounted(() => {
         await batteryStore.update()
     })()
 })
+
+const sortedBatteries = computed(() => {
+    return [...batteryStore.table].sort((a, b) => {
+        return b.level - a.level
+    }).sort((a, b) => {
+        return a.node_type.localeCompare(b.node_type)
+    })
+})
 </script>
 
 <template>
-    <main class="container">
-        <table>
+    <main class="container-fluid">
+        <table class="striped">
             <thead>
                 <tr>
                     <th scope="col">名前</th>
+                    <th scope="col">種類</th>
                     <th scope="col">充電状況</th>
                     <th scope="col">満充電までの時間</th>
                     <th scope="col">充電切れまでの時間</th>
@@ -30,8 +39,9 @@ onMounted(() => {
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="battery in batteryStore.table" v-bind:key="battery.node_id">
+                <tr v-for="battery in sortedBatteries" v-bind:key="battery.node_id">
                     <th scope="row">{{ battery.node_name }}</th>
+                    <td>{{ battery.node_type }}</td>
                     <td>{{ battery.charging ? "充電中" : "充電していません" }}</td>
                     <td>{{ (battery.charging_time / 3600).toFixed(2) }} 時間</td>
                     <td>{{ (battery.discharging_time / 3600).toFixed(2) }} 時間</td>
